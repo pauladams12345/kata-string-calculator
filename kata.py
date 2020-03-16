800c3b022089e50b542ab7c0336000843bca1235
Paul Adams <pauladams12345@gmail.com>
2020-03-15 20:57:03 -0400

    Step 1: Create skeleton

---
+++ kata.py
@@ -0,0 +1,16 @@
+import unittest
+
+# Calculates the sum of up to two numbers. If no
+# numbers are supplied, returns 0
+# Parameters:
+# 	s: a string of integers separated by a comma
+# Returns:
+#	Sum of supplied numbers, or 0 if input is empty
+def add(s):
+	pass
+
+class TestAdd(unittest.TestCase):
+	pass
+
+if __name__ == '__main__':
+	unittest.main()
